c52760d945d4a936f6fa46b53882077dd945a850
Harshad Nawathe <harshadn@thoughtworks.com>
2022-09-16 13:26:49 +0530

    Wire OpenAOpenApiEntityProcessor in catalog backend

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -2,12 +2,14 @@ import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
+import { OpenApiEntityProcessor } from '@thoughtworks-backstage/plugin-openapi-definition-sync-backend';
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
   builder.addProcessor(new ScaffolderEntitiesProcessor());
+  builder.addProcessor(new OpenApiEntityProcessor(env))
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
   return router;
